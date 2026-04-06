3132866f7464d4e731157d0416682d2fe12a9a21
ksanuraag <ksanuraag@gmail.com>
2026-04-06 18:32:18 +0530

    s3 storage

--- project/settings.py
+++ project/settings.py
@@ -179,4 +179,11 @@ AWS_QUERYSTRING_AUTH = False
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
